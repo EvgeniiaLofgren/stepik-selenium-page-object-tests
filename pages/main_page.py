4bb6cd86d72dd8e5e83d446998c8bb1d0b3e586a
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-22 22:30:42 +0200

    Update main_page.py
    Adding assertion for NoSuchElementException

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,9 @@
-from .base_page import BasePage
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
-class MainPage(BasePage): 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+class MainPage(BasePage):
+    def should_be_login_link(self):
+        try:
+            self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        except NoSuchElementException:
+            assert False, "Login link is not present on the page"
