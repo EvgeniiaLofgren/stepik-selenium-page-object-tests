ecdb5115be048a7f78707a364344e291a6e83ac2
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 13:33:00 +0200

    Update product_page.py
    feat: add negative test cases for product page success message
    
    - Implement is_not_element_present and is_disappeared methods in BasePage
    - Add assertions to ProductPage for checking absence and disappearance of success message
    - Write 3 test cases for these scenarios
    - Mark known failing test with @pytest.mark.xfail

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,27 +1,15 @@
-from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
-    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
-    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1) .alertinner strong")
-    MESSAGE_PRICE = (By.CSS_SELECTOR, "#messages .alert-info .alertinner strong")
-
     def add_product_to_basket(self):
-        self.browser.find_element(*self.ADD_TO_BASKET_BUTTON).click()
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_button.click()
 
-    def get_product_name(self):
-        return self.browser.find_element(*self.PRODUCT_NAME).text
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
 
-    def get_product_price(self):
-        return self.browser.find_element(*self.PRODUCT_PRICE).text
-
-    def is_message_about_adding_present(self):
-        return self.is_element_present(*self.MESSAGE_ABOUT_ADDING)
-
-    def get_name_in_message(self):
-        return self.browser.find_element(*self.MESSAGE_ABOUT_ADDING).text
-
-    def get_price_in_message(self):
-        return self.browser.find_element(*self.MESSAGE_PRICE).text
+    def message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message did not disappear"
