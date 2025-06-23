adbd542d0e62d60667bfb1abfe426d2517ff7ecb
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 12:36:21 +0200

    Update test_product_page.py
    Add independent product page test with dynamic verification of product name and price
    
    - Created ProductPage with methods to get product name and price dynamically
    - Added checks for product name and basket price using actual page data (no hardcoded values)
    - Implemented solve_quiz_and_get_code method in BasePage for promo code alert handling
    - Wrote test_guest_can_add_product_to_basket in test_product_page.py using Page Object pattern
    - Test supports multiple product URLs with promo codes via parametrization

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,20 @@
+import pytest
 from pages.product_page import ProductPage
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', [
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019",
+    "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+])
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
+
+    product_name = page.get_product_name()
+    product_price = page.get_product_price()
+
     page.add_product_to_basket()
+
     page.solve_quiz_and_get_code()
-    page.should_be_correct_product_added()
-    page.should_be_correct_basket_total()
+
+    page.should_be_correct_product_name_in_message(product_name)
+    page.should_be_correct_basket_price(product_price)
