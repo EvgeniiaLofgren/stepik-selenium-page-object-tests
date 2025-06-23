798a3958503d988e7851b911ecf45b4fc6ef3f8a
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 22:24:02 +0200

    Update main_page.py
    Add basket page tests to verify basket is empty from main and product pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,11 @@
-from .base_page import BasePage
+from .pages.main_page import MainPage
+from .pages.basket_page import BasketPage
 
-class MainPage(BasePage):
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty()
+    basket_page.should_be_empty_text_present()
