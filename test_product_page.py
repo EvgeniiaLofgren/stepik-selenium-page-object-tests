f8f4e3bb3df05eb275f4633afabe8755d7031d14
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 22:34:35 +0200

    Update test_product_page.py
    typo fixed

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,5 @@
 from pages.product_page import ProductPage
-from .pages.basket_page import BasketPage
+from pages.basket_page import BasketPage
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
