f8de78e3e0ed8dbc84e3fe50539540c3a678de86
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 12:47:56 +0200

    Update test_product_page.py
    Added parameterized test with promo=offer0-9 check, bug marked as xfail for offer9

--- test_product_page.py
+++ test_product_page.py
@@ -1,20 +1,33 @@
 import pytest
 from pages.product_page import ProductPage
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 @pytest.mark.parametrize('link', [
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019",
-    "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+    pytest.param(
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
+        marks=pytest.mark.xfail(reason="Известный баг при promo=offer9")
+    ),
 ])
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
-
-    product_name = page.get_product_name()
-    product_price = page.get_product_price()
-
     page.add_product_to_basket()
-
     page.solve_quiz_and_get_code()
-
-    page.should_be_correct_product_name_in_message(product_name)
-    page.should_be_correct_basket_price(product_price)
+    
+    product_name = page.get_product_name()
+    success_message = page.get_success_message()
+    assert product_name == success_message, "Название товара в сообщении не совпадает с добавленным товаром"
+    
+    product_price = page.get_product_price()
+    basket_price = page.get_basket_total()
+    assert product_price == basket_price, "Цена товара не совпадает со стоимостью корзины"
