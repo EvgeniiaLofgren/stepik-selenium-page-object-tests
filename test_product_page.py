02bd18a16dced9f37da28928168bcd2cb31b64d0
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 13:33:34 +0200

    Update test_product_page.py
    feat: add negative test cases for product page success message
    
    - Implement is_not_element_present and is_disappeared methods in BasePage
    - Add assertions to ProductPage for checking absence and disappearance of success message
    - Write 3 test cases for these scenarios
    - Mark known failing test with @pytest.mark.xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,28 +1,23 @@
 import pytest
-from pages.product_page import ProductPage
+from .pages.product_page import ProductPage
 
-@pytest.mark.parametrize('link', [
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-    pytest.param(
-        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
-        marks=pytest.mark.xfail(reason="Known bug with promo=offer9")
-    )
-])
-def test_guest_can_add_product_to_basket(browser, link):
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+@pytest.mark.need_review
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    product_name = page.get_product_name()
-    product_price = page.get_product_price()
-    assert page.is_message_about_adding_present()
-    assert page.get_name_in_message() == product_name
-    assert page.get_price_in_message() == product_price
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.message_should_disappear()
