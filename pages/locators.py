c39454069d0cb598b7f20af58376177ca4cf717a
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 13:36:43 +0200

    Update locators.py
    refactor: move login link logic to BasePage and test login navigation from ProductPage
    
    - Add BasePageLocators with login link locators
    - Add go_to_login_page and should_be_login_link to BasePage
    - Add tests to check login link visibility and functionality on ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -1,16 +1,16 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+class MainPageLocators():
+    pass  # оставить, если планируется добавить что-то позже
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.ID, "login_form")
-    REGISTER_FORM = (By.ID, "register_form")
-    
-class ProductPageLocators:
-    ADD_TO_BASKET = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
-    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
-    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
-    MESSAGE_PRODUCT_ADDED = (By.CSS_SELECTOR, '#messages .alert-success .alertinner strong')
-    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, '.alert-info .alertinner strong')
-    
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
