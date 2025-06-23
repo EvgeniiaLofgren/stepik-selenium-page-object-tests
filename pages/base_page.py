78d0d360b88ae4243d53055518b67ee644a00418
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 13:37:13 +0200

    Update base_page.py
    refactor: move login link logic to BasePage and test login navigation from ProductPage
    
    - Add BasePageLocators with login link locators
    - Add go_to_login_page and should_be_login_link to BasePage
    - Add tests to check login link visibility and functionality on ProductPage

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,9 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class BasePage:
+from .locators import BasePageLocators
+
+class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
@@ -20,8 +22,7 @@ class BasePage:
 
     def is_not_element_present(self, how, what, timeout=4):
         try:
-            WebDriverWait(self.browser, timeout).until(
-                EC.presence_of_element_located((how, what)))
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return True
         return False
@@ -33,3 +34,10 @@ class BasePage:
         except TimeoutException:
             return False
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
