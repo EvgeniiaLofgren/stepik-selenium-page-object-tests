1579f992c02fbc90cfa8617de315affcca30959d
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 22:18:57 +0200

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,5 @@
 import pytest
-from .pages.product_page import ProductPage
+from pages.product_page import ProductPage
 
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
