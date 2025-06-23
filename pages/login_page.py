ce41b086a89d2b770dd57184f0689b5da56afc30
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 22:44:55 +0200

    Update login_page.py
    Added TestUserAddToBasketFromProductPage class with user registration
    
    - Added setup with automatic registration in tests for registered users
    - Added register_new_user method in LoginPage
    - Added user authorization check in BasePage
    - Added USER_ICON locator for authorization check

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,12 +1,19 @@
+from selenium.webdriver.common.by import By
 from .base_page import BasePage
-from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
-    def should_be_login_url(self):
-        assert "login" in self.browser.current_url, "URL does not contain 'login'"
+    REGISTER_EMAIL = (By.ID, "id_registration-email")
+    REGISTER_PASSWORD1 = (By.ID, "id_registration-password1")
+    REGISTER_PASSWORD2 = (By.ID, "id_registration-password2")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
 
-    def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+    def register_new_user(self, email, password):
+        email_input = self.browser.find_element(*self.REGISTER_EMAIL)
+        password1_input = self.browser.find_element(*self.REGISTER_PASSWORD1)
+        password2_input = self.browser.find_element(*self.REGISTER_PASSWORD2)
+        register_btn = self.browser.find_element(*self.REGISTER_BUTTON)
 
-    def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+        email_input.send_keys(email)
+        password1_input.send_keys(password)
+        password2_input.send_keys(password)
+        register_btn.click()
