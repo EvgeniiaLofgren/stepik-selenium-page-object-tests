9efd1cfe6595617a319450fc00c0d989004cec39
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 22:32:09 +0200

    Update locators.py
    Added basket page locators

--- pages/locators.py
+++ pages/locators.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn")  # ссылка на корзину в шапке
 
 class MainPageLocators():
     pass  # оставить, если планируется добавить что-то позже
@@ -14,3 +15,8 @@ class LoginPageLocators():
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+
+class BasketPageLocators():
+    BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
+    EMPTY_BASKET_TEXT = (By.CSS_SELECTOR, "#content_inner > p")  # текст «Ваша корзина пуста»
+
