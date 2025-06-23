a005545e838b5dcac4a26982b8971f592ea63a76
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 13:00:38 +0200

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,5 @@
 import pytest
-from pages.product_page import ProductPage
-from selenium.common.exceptions import NoAlertPresentException
-import math
+from .pages.product_page import ProductPage
 
 @pytest.mark.parametrize('link', [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -15,19 +13,16 @@ import math
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
     pytest.param(
         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
-        marks=pytest.mark.xfail(reason="Известный баг при promo=offer9")
-    ),
+        marks=pytest.mark.xfail(reason="Known bug with promo=offer9")
+    )
 ])
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-    
     product_name = page.get_product_name()
-    success_message = page.get_success_message()
-    assert product_name == success_message, "Название товара в сообщении не совпадает с добавленным товаром"
-    
     product_price = page.get_product_price()
-    basket_price = page.get_basket_total()
-    assert product_price == basket_price, "Цена товара не совпадает со стоимостью корзины"
+    assert page.is_message_about_adding_present()
+    assert page.get_name_in_message() == product_name
+    assert page.get_price_in_message() == product_price
