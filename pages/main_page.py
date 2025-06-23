fedbf854cc53627110e2017797fd81870d153bc0
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 22:27:56 +0200

    Update main_page.py
    reverted back incorrect commit

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,5 @@
-from .pages.main_page import MainPage
-from .pages.basket_page import BasketPage
+from .base_page import BasePage
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_empty()
-    basket_page.should_be_empty_text_present()
+class MainPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
