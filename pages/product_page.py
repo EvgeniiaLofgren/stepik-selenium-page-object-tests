b109eb7b9acf6a85705a181cfdcb9d078db8d02e
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-23 13:39:17 +0200

    Update product_page.py
    refactor: move login link logic to BasePage and test login navigation from ProductPage
    
    - Add BasePageLocators with login link locators
    - Add go_to_login_page and should_be_login_link to BasePage
    - Add tests to check login link visibility and functionality on ProductPage

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,13 +3,5 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
-        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
-        add_button.click()
-
-    def should_not_be_success_message(self):
-        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
-            "Success message is presented, but should not be"
-
-    def message_should_disappear(self):
-        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
-            "Success message did not disappear"
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        button.click()
