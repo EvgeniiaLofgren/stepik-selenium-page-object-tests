ccfd54befbcb470ec76cc8c44e0b712a4b508891
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-24 09:58:37 +0200

    Update basket_page.py
    Add basket_page: empty basket checks

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -4,8 +4,8 @@ from .locators import BasketPageLocators
 class BasketPage(BasePage):
     def should_be_empty(self):
         assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), \
-            "Basket is not empty, but should be"
+            "Basket is not empty but should be"
 
-    def should_be_empty_text_present(self):
+    def should_be_message_about_empty(self):
         assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_TEXT), \
-            "Empty basket text is not presented"
+            "Empty basket message is not present"
