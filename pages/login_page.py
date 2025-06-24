8c029ae537dcdc037d93a566a424e5ebe5cad2c8
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-24 10:01:35 +0200

    Update login_page.py
    Implement login_page with form and URL checks

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,19 +1,20 @@
-from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
-    REGISTER_EMAIL = (By.ID, "id_registration-email")
-    REGISTER_PASSWORD1 = (By.ID, "id_registration-password1")
-    REGISTER_PASSWORD2 = (By.ID, "id_registration-password2")
-    REGISTER_BUTTON = (By.NAME, "registration_submit")
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
 
-    def register_new_user(self, email, password):
-        email_input = self.browser.find_element(*self.REGISTER_EMAIL)
-        password1_input = self.browser.find_element(*self.REGISTER_PASSWORD1)
-        password2_input = self.browser.find_element(*self.REGISTER_PASSWORD2)
-        register_btn = self.browser.find_element(*self.REGISTER_BUTTON)
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, \
+            "Login URL does not contain 'login'"
 
-        email_input.send_keys(email)
-        password1_input.send_keys(password)
-        password2_input.send_keys(password)
-        register_btn.click()
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+            "Login form is not present"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
+            "Register form is not present"
