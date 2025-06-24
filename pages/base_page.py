3d09a8bb7c3cd057eb2dddd79a0b894def2ed46c
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-24 10:27:11 +0200

    Update base_page.py
    fixed a typo

--- pages/base_page.py
+++ pages/base_page.py
@@ -52,7 +52,7 @@ class BasePage:
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x)))))
+        answer = str(math.log(abs(12 * math.sin(float(x)))))
         alert.send_keys(answer)
         alert.accept()
         try:
