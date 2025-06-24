95d81837190949da85b49e9071e0fe76ea6a5e68
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-24 11:03:24 +0200

    Update conftest.py
    fixing tests

--- conftest.py
+++ conftest.py
@@ -1,25 +1,28 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from pages.login_page import LoginPage
+import time
+import uuid
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en',
-                     help="Choose browser language")
-    parser.addoption('--browser_name', action='store', default='chrome',
-                     help="chrome or firefox")
+    parser.addoption("--language", action="store", default="en")
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption('browser_name')
-    language = request.config.getoption('language')
-
-    if browser_name == 'chrome':
-        options = Options()
-        if language:
-            options.add_experimental_option('prefs', {'intl.accept_languages': language})
-        browser = webdriver.Chrome(options=options)
-    else:
-        pytest.skip("Only Chrome is supported")
-
+    options = Options()
+    user_language = request.config.getoption("language")
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
+
+@pytest.fixture(scope="function")
+def setup_user(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    email = f"{uuid.uuid4()}@fakemail.org"
+    password = str(time.time())
+    page.register_new_user(email, password)
+    page.should_be_authorized_user()
