72d64bbe761dfd70b3305e352ef9309cb34033b7
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-24 10:00:45 +0200

    Update test_login_page.py
    Add login page checks for forms and URL

--- test_login_page.py
+++ test_login_page.py
@@ -1,13 +1,9 @@
 import pytest
-from pages.main_page import MainPage
 from pages.login_page import LoginPage
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
+def test_login_page_has_login_form(browser):
+    page = LoginPage(browser, "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/")
     page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_url()
-    login_page.should_be_login_form()
-    login_page.should_be_register_form()
+    page.should_be_login_form()
+    page.should_be_register_form()
+    page.should_be_login_url()
