d1bf05a5d1f814f043458d588243f153e259c08b
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-24 10:02:05 +0200

    Update conftest.py
    pdate conftest: language option and Chrome setup only

--- conftest.py
+++ conftest.py
@@ -2,11 +2,24 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose browser language")
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="chrome or firefox")
+
 @pytest.fixture(scope="function")
-def browser():
-    options = Options()
-    options.add_argument("--start-maximized")
-    browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(10)  # добавлено ожидание
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    language = request.config.getoption('language')
+
+    if browser_name == 'chrome':
+        options = Options()
+        if language:
+            options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        pytest.skip("Only Chrome is supported")
+
     yield browser
     browser.quit()
