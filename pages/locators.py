2b8fd7962fccf6bf89e114c9567fb8c444a03ade
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-24 11:13:28 +0200

    Update locators.py
    fixing test

--- pages/locators.py
+++ pages/locators.py
@@ -4,7 +4,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
-    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn-default")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a.btn")
 
 class MainPageLocators():
     pass  # for future use
