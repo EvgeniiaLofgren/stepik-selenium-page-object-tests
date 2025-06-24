278e17b8bea10c090430f9e79af3f0f69b11f607
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-24 09:57:53 +0200

    Update product_page.py
    Improved product_page: add to basket, success message checks

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,9 +1,6 @@
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from .base_page import BasePage
 from .locators import ProductPageLocators
+from selenium.common.exceptions import NoSuchElementException
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
@@ -18,11 +15,6 @@ class ProductPage(BasePage):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
 
-    def is_not_element_present(self, how, what, timeout=4):
-        """Проверяет, что элемент НЕ появляется на странице в течение timeout секунд."""
-        try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return True  # элемент не появился, как и ожидалось
-        return False  # элемент появился
-
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message did not disappear as expected"
