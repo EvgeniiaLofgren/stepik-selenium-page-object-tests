a9672e9424353c8f1c38ce869290f958171482ad
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-24 11:11:38 +0200

    Update base_page.py
    fixing test

--- pages/base_page.py
+++ pages/base_page.py
@@ -33,19 +33,14 @@ class BasePage:
             return False
         return True
 
-    def solve_quiz_and_get_code(self):
-        try:
-            alert = self.browser.switch_to.alert
-            x = alert.text.split("x = ")[1]
-            answer = str(math.log(abs(12 * math.sin(float(x)))))
-            alert.send_keys(answer)
-            alert.accept()
-            try:
-                alert = self.browser.switch_to.alert
-                alert_text = alert.text
-                print(f"Your code: {alert_text}")
-                alert.accept()
-            except NoAlertPresentException:
-                print("No second alert presented")
-        except NoAlertPresentException:
-            assert False, "No alert presented"
+   def solve_quiz_and_get_code(self):
+    alert = self.browser.switch_to.alert
+    x = alert.text.split(" ")[2]
+    answer = str(math.log(abs((12 * math.sin(float(x))))))
+    alert.send_keys(answer)
+    alert.accept()
+    try:
+        alert = self.browser.switch_to.alert
+        alert.accept()
+    except NoAlertPresentException:
+        pass
