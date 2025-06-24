3d470ec74a5aecfc5a70cb4a20da118ae14c2892
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-24 11:02:26 +0200

    Update test_main_page.py
    fixing tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,17 @@
 import pytest
 from pages.main_page import MainPage
-from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)
+        page = MainPage(browser, "http://selenium1py.pythonanywhere.com/")
         page.open()
         page.go_to_login_page()
+        login = LoginPage(browser, browser.current_url)
+        login.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)
+        page = MainPage(browser, "http://selenium1py.pythonanywhere.com/")
         page.open()
         page.should_be_login_link()
-        
