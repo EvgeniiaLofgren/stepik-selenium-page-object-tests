2043c383373b2d17e04dbd62fd20a4ca57841f8a
Evgeniia Löfgren <59977428+EvgeniiaLofgren@users.noreply.github.com>
2025-06-29 12:01:53 +0200

    Update test_product_page.py
    fixing promo code

--- test_product_page.py
+++ test_product_page.py
@@ -7,6 +7,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
